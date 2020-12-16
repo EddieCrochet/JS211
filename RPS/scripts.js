b10d9cd6eb8c700f02b49ec6ba7221d5b274a3da
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-12-16 14:32:13 -0600

    multiple settimeout functions to add suspense...

--- RPS/scripts.js
+++ RPS/scripts.js
@@ -1,7 +1,21 @@
 'use strict'
 
-const dropItem = document.getElementsByClassName("drpItem");
+const p1fg = document.getElementById("p1fg");
+const p2fg = document.getElementById("p2fg");
+const h1fg = document.getElementById("h1fg");
+const h2fg = document.getElementById("h2fg");
 
 const drpItemSelected = (e) => {
     let dropSelected = e.innerHTML;
+    console.log(document.getElementsByClassName("p1fg"))
+    p1fg.classList.remove("hide");
+    setTimeout(function() {
+        h1fg.innerHTML= e.innerHTML;
+    }, 1000);
+    setTimeout(function() {
+        p2fg.classList.remove("hide");
+    }, 2000);
+    setTimeout(function() {
+        h2fg.innerHTML= e.innerHTML;
+    }, 3000);
 }
