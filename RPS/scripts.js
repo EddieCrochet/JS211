23aa320de09a5a0b5d06e3a518e3112ed71a58a7
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-12-17 13:54:02 -0600

    had to edit the logic for randum computer selection, aslo adding a function to handle comparing theh ands

--- RPS/scripts.js
+++ RPS/scripts.js
@@ -5,9 +5,8 @@ const p2fg = document.getElementById("p2fg");
 const h1fg = document.getElementById("h1fg");
 const h2fg = document.getElementById("h2fg");
 
+//this function assigns the user and comp to their choices
 const drpItemSelected = (e) => {
-    let dropSelected = e.innerHTML;
-    console.log(document.getElementsByClassName("p1fg"))
     p1fg.classList.remove("hide");
     setTimeout(function() {
         h1fg.innerHTML= e.innerHTML;
@@ -16,18 +15,26 @@ const drpItemSelected = (e) => {
         p2fg.classList.remove("hide");
     }, 2000);
     setTimeout(function() {
-        h2fg.innerHTML= e.innerHTML;
+        h2fg.innerHTML= compRPS();
     }, 3000);
+    determineWinner();
 }
 
+//compare the two selections and determine winner
+const determineWinner = () => {
+
+}
+
+//function for the computer to select an option for the game
 const compRPS = () => {
     let RPS;
-    let ranNum = Math.floor(Math.random() * 2);
-    if(ranNum = 0) {
+    let ranNum = Math.floor(Math.random() * 3);
+    console.log(ranNum)
+    if(ranNum == 0) {
         RPS = "Rock";
-    } else if(ranNum=1) {
+    } else if(ranNum==1) {
         RPS = "Paper";
-    } else if (ranNum = 2) {
+    } else if (ranNum == 2) {
         RPS = "Scissors";
     }
     return RPS;
